5dd8c2ebe2c2f9eb1febe10554f15d45b77c0219
Dillon J Byrne <djb8tc@mail.missouri.edu>
2014-12-13 18:36:59 -0600

    Index up and running as something resembling a webpage

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>Dillon Byrne</title>
+		<style>
+			* {background-color: #2c3e50; color: white; font-family: "Trebuchet MS"}
+			ul {list-style-type: none; margin: 0; padding: 0;}
+			a {float: right; display: block; text-align: center; background-color: rgba(0, 0, 0, 0.5); margin: 2px; border-radius: 10px; text-decoration: none; font-size: 20pt; width: 150px}
+			#Header {float: left; display: block; font-weight: bold; font-size: 40pt;
+		</style>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	</head>
+
+	<body>
+		<div id="header"></div>
+		<script>
+			$("#header").load("header.html");
+		</script>
+	</body>
+</html>
